46ccb86ff8a8b1603b58afb878178ed6d3e5a260
Thiago <thiago_sl2@htomail.com>
2024-04-17 01:38:55 -0300

    fix

--- db/config/db_config.js
+++ db/config/db_config.js
@@ -7,11 +7,11 @@ dotenv.config();
 
 export const DATABASE = new Sequelize({
 
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
+    database: `${process.env.DB_DATABASE}`,
+    username: `${process.env.DB_USERNAME}`,
+    password: `${process.env.DB_PASSWORD}`,
+    host: `${process.env.DB_HOST}`,
+    dialect: 'mysql',
   dialectModule: mysql2,
   benchmark: true
 });
